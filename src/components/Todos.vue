d90f1899101e7f9a9832ee75ea54062bf07f1e34
tushar <tushar@metadesignsolutions.co.uk>
2020-04-02 12:14:14 +0530

    added filter ,addtodo,state store,update todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,99 @@
 <template>
-    <div>
-        <h3>Todos</h3>
+  <div>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double Click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span>=Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span>=Complete
+      </span>
     </div>
+    <div class="todos">
+      <div
+        class="todo"
+        v-for="todo in allTodos"
+        :key="todo.id"
+        @dblclick="onDblClick(todo)"
+        v-bind:class="{'is-complete':todo.completed}"
+      >
+        {{todo.title}}
+        <i class="fas fa-trash-alt" @click="deleteTodo(todo.id)"></i>
+      </div>
+    </div>
+  </div>
 </template>
-<script>
-export default {
-    name:"Todos"
-}
-</script>
-<style scoped>
 
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Todos",
+  methods: {
+    ...mapActions(["getTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updatedTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+
+      this.updateTodo(updatedTodo);
+    }
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.getTodos();
+  }
+};
+</script>
+
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,16 +1,70 @@
-import axios from 'axios';
+import axios from "axios";
 
-const state={};
+const state = {
+  todos: []
+};
 
-const getters={};
+const getters = {
+  allTodos: state => state.todos
+};
 
-const actions={};
+const actions = {
+  async getTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    commit("setTodos", response.data);
+  },
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      { title, completed: false }
+    );
+    commit("newTodo", response.data);
+  },
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    commit("removeTodo", id);
+  },
+  async filterTodos({ commit }, event) {
+    const limit = parseInt(event.target.value);
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+    );
+    console.log(limit);
 
-const mutations={};
+    commit("setTodos", response.data);
+  },
+  async updateTodo({ commit }, updatedTodo) {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`,
+      updatedTodo
+    );
+    commit("updateTodo", response.data);
+  }
+};
 
-export default{
-    state,
-    getters,
-    actions,
-    mutations
-}
+const mutations = {
+  setTodos: (state, todos) => {
+    state.todos = todos;
+  },
+  newTodo: (state, todo) => {
+    state.todos.unshift(todo);
+  },
+  removeTodo: (state, id) => {
+    state.todos = state.todos.filter(todo => todo.id !== id);
+  },
+  updateTodo: (state, updatedTodo) => {
+    const index = state.todos.findIndex(todo => todo.id === updatedTodo.id);
+    if (index !== -1) {
+      state.todos.splice(index, 1, updatedTodo);
+    }
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
